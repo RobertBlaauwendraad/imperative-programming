7134bfd93e127a2a1a994be0e230b013f17eda86
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2021-09-10 12:18:22 +0200

    Refactored code

--- agent.cpp
+++ agent.cpp
@@ -7,81 +7,88 @@ void test_agent() {
 
 // Pick up ball and step, repeat until in front of wall then turn right
 void get_string_of_balls() {
-  while(!in_front_of_wall()) {
+  while(!in_front_of_wall() && on_ball()) {
     get_ball();
     step();
   }
-  turn_right();
 }
 
-// get string of balls, repeat until all balls are picked up
+// Get string of balls, repeat until all balls are picked up
 void string_agent() {
   while(on_ball()){
     get_string_of_balls();
+    turn_right();
   }
 }
 
-void walk_to_first_ball(){
+// Step until on ball
+void travel_to_first_ball(){
     while(!on_ball()){
         step();
     }
 }
 
-void traverse(){
-    while(true){
-        //if Charles is on a ball but not in front of a wall he should
-        //get the ball and take a step forward
-        if(on_ball() && !in_front_of_wall()){
-            get_ball();
-            step();
-
-        //if Charles is on a ball and also in front of a wall he should get that ball
-        //and move one row down
-        } else if(on_ball() && in_front_of_wall()){
-            get_ball();
-            turn_right();
-            step();
-            turn_right();
-
-        //if Charles is not on a ball and also not in front of a wall he
-        //should turn around and go one row down
-        } else if(!on_ball() && !in_front_of_wall()){
-            turn_right();
-            turn_right();
-            while(!in_front_of_wall()){
-                step();
-            }
-            turn_right();
-            step();
-            turn_right();
-
-            //this only gets triggered when he is done and should return home
-            if(!on_ball()){
-
-                while(!north()){
-                    turn_right();
-                }
-                while(!in_front_of_wall()){
-                    step();
-                }
-                turn_left();
-                while(!in_front_of_wall()){
-                    step();
-                }
-                turn_left();
-                turn_left();
-            }
-        }
-       
-
-    }     
+// step until in front of wall
+void travel_to_wall() {
+    while (!in_front_of_wall())
+    {
+        step();
+    }
 }
 
+// Prepare Charles to pick up next string of balls
+void travel_to_next_row() {
+    if(in_front_of_wall() && on_ball()) {
+        get_ball();
+        turn_right();
+        step();
+        turn_right();
+        // Skip first ball to keep loop going
+        step();
+    }
+}
+
+// After picking up string of balls, return to wall
+void return_to_wall() {
+    if(!in_front_of_wall() && !on_ball()) {
+        turn_right();
+        turn_right();
+        travel_to_wall();
+    }
+}
+
+// Rotates Charles facing north
+void rotate_north() {
+    while (!north())
+    {
+        turn_right();
+    }
+}
+
+// Return back to starting position
+void return_to_starting_position() {
+    rotate_north();
+    travel_to_wall();
+    turn_left();
+    travel_to_wall();
+    turn_left();
+    turn_left();
+}
+
+// Main agent
 void chaos_agent() {
-
-    walk_to_first_ball();
-    traverse(); 
-
+    // First string of balls is picked up from left to right
+    // After that the same order of functions can be used
+    travel_to_first_ball();
+    get_string_of_balls();
+    // Repeat functions until all balls are picked up
+    while (on_ball())
+    {
+        travel_to_next_row();
+        get_string_of_balls();
+        return_to_wall();
+    }
+    return_to_starting_position();
 }
 
 
