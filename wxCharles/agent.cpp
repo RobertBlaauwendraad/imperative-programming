56cf00ceb6d87cf5d39dfa8f3a07ab401c4d3a62
shnvandiepen1 <s.vandiepen@student.maastrichtuniversity.nl>
2021-09-16 11:10:27 +0200

    part 1 added

--- wxCharles/agent.cpp
+++ wxCharles/agent.cpp
@@ -5,9 +5,73 @@ void test_agent() {
 
 }
 
-void path_agent() {
-
+//go to the location left of the previous ball
+void look_left(){
+    turn_left();
+    step();
 }
+
+//go to the location right of the previous ball
+void look_right(){
+    turn_right();
+    turn_right();
+    step();
+    step();
+}
+
+//go back to previous ball's location
+void to_origin(){
+    turn_right();
+    turn_right();
+    step();
+    turn_right();
+    turn_right();
+}
+
+//as long as Charles is on a ball it picks it up and step forward
+void get_balls(){
+    while(on_ball()){
+        get_ball();
+        step();
+    }
+}
+
+bool done = false;
+
+//tries to find the next direction of the ball trail
+//by going back to previous ball and looking left and right of it
+void find_direction(){
+    to_origin();
+    look_left();
+    if(on_ball()){
+        get_balls();
+    } else {
+        look_right();        
+        if(on_ball()){
+            get_balls();
+        } else {
+            while(!north()){
+                turn_right();
+            }
+            turn_right();
+            //only true when there is no ball neither left or right
+            //from the last ball, so when the trail is done
+            done = true;
+        }
+    }
+}
+
+
+
+void path_agent() {
+    while(!done){
+        if(on_ball()){
+            get_balls();
+        } 
+        find_direction();
+    } 
+}
+
 
 // step until in front of wall
 void travel_to_wall() {
