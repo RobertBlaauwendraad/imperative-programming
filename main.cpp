4ee5758b9dc26306adc95e920a37bb83e306ad89
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2021-10-15 20:58:34 +0200

    Part 1 done

--- main.cpp
+++ main.cpp
@@ -12,18 +12,70 @@ const int NO_OF_ROWS        = 40 ;                // the number of rows (height)
 const int NO_OF_COLUMNS     = 60 ;                // the number of columns (width) of the universe (both on file and screen)
 const int ROWS              = NO_OF_ROWS    + 2 ; // the number of rows in a universe array, including the 'frame' of dead cells
 const int COLUMNS           = NO_OF_COLUMNS + 2 ; // the number of columns in a universe array, including the 'frame' of dead cells
-
+const string FILENAME = "build/glider0.txt";
 
 //  Part 1: setting the scene
 bool read_universe_file (ifstream& inputfile, Cell universe [ROWS][COLUMNS])
 {
-    // pre-conditions, post-conditions, implementation
+    // Precondition:
+    assert(inputfile.is_open());
+    /* Post-condition:
+    * Get universe from extern file and put it in universe array.
+    */
+
+    char cell;
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            if (i == 0 || i == ROWS - 1 || j == 0 || j == COLUMNS - 1)
+            {
+                universe[i][j] = (Cell)0;
+            }
+        }
+    }
+
+    for (int i = 1; i <= NO_OF_ROWS; i++)
+    {
+        for (int j = 1; j <= NO_OF_COLUMNS; j++)
+        {
+            inputfile.get(cell);
+            if (cell == LIVE)
+            {
+                universe[i][j] = (Cell)1;
+            } else if (cell == DEAD)
+            {
+                universe[i][j] = (Cell)0;
+            } else {
+                j--;
+            }
+        }
+    }
     return false;
 }
 
 void show_universe (Cell universe [ROWS][COLUMNS])
 {
-    // pre-conditions, post-conditions, implementation
+    // Precondition:
+    assert(true);
+    /* Post-condition:
+    * Show the universe array in the console.
+    */
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            if (universe[i][j] == (Cell)1)
+            {
+                // cout << '(' << i << ',' << j << ')';
+                cout << '*';
+            } else if (universe[i][j] == (Cell)0) {
+                cout << '.';
+            }
+        }
+        cout << endl;
+    }
 }
 
 //  Part 2: the next generation
@@ -35,6 +87,16 @@ void next_generation (Cell now [ROWS][COLUMNS], Cell next [ROWS][COLUMNS])
 #ifndef TESTING
 int main ()
 {
+    Cell universe[ROWS][COLUMNS];
+    ifstream inputfile(FILENAME);
+    if(inputfile.fail()) {
+        cout << FILENAME << " could not be opened\n";
+        return 1;
+    }
+
+    read_universe_file(inputfile, universe);
+    
+    show_universe(universe);
     return 0;
 }
 #endif
