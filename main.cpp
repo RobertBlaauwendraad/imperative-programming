b6ececcebebf70525b50933f849c20eeb8b1457e
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2021-12-10 16:40:22 +0100

    Added all prices positive check

--- main.cpp
+++ main.cpp
@@ -4,19 +4,42 @@
 #include <string>
 using namespace std;
 
-int gifts(vector<string> wishlist, int budget) {
+struct Gift {
+  int price;
+  string category;
+  string name;
+};
+
+bool all_prices_positive(vector<Gift> wishlist) {
   // Precondition
-  assert(budget >= 0);
+  assert(true);
+  // Post-condition
+  // Checks whether all prices on wishlist are positive
+  bool all_positive = true;
+  for (int i = 0; i < static_cast<int>(size(wishlist)); i++) {
+    if (wishlist[i].price < 0) {
+      all_positive = false;
+    }
+  }
+  return all_positive;
+}
+
+int gifts(vector<Gift> wishlist, int gift, int budget) {
+  // Precondition
+  assert(all_prices_positive(wishlist) && gift >= 0);
   // Post-condition
   // Returns remaining budget after recursively "buying" gifts from the given wishlist
 
   // Base cases
-  if (budget == 0 || wishlist.empty()) {
-    return budget;
+  if (budget == 0) {
+    return 1;
+  } else if (budget < 0 || gift >= static_cast<int>(size(wishlist))) {
+    return 0;
+  } else {
+    int with_gift = gifts(wishlist, gift+1, budget - wishlist[gift].price);
+    int without_gift = gifts(wishlist, gift+1, budget);
+    return with_gift + without_gift;
   }
-
-  // TODO: Write algorithm
-  return budget;
 }
 
 #ifndef TESTING
--- main_test.cpp
+++ main_test.cpp
@@ -2,10 +2,20 @@
 #include "gtest/gtest.h"
 
 // TODO: Write correct tests
-TEST(gifts_test, zero_budget) {
-  EXPECT_EQ(gifts({"Playstation 5"}, 0), 0);
+TEST(all_prices_positive, negative_price) {
+  Gift gift = {.price= -100};
+  EXPECT_EQ(all_prices_positive({gift}), false);
 }
 
-TEST(gifts_test, empty_wishlist) {
-  EXPECT_EQ(gifts({}, 3000), 3000);
+TEST(all_prices_positive, positive_price) {
+  Gift gift = {.price= 100};
+  EXPECT_EQ(all_prices_positive({gift}), true);
 }
+
+//TEST(gifts_test, zero_budget) {
+//  EXPECT_EQ({}, 0), 0);
+//}
+//
+//TEST(gifts_test, empty_wishlist) {
+//  EXPECT_EQ(gifts({}, 3000), 3000);
+//}
