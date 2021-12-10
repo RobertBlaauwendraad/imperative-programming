867557af2da9db3f8cf59d83272fd2d5eb3803f4
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2021-12-10 16:15:31 +0100

    Added base cases

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
-using namespace std;
+#include <iostream>
 #include <cassert>     /* assert */
+#include <vector>
+#include <string>
+using namespace std;
 
-int gifts() {
+int gifts(vector<string> wishlist, int budget) {
   // Precondition
-  // TODO: Write precondition
-  assert(true);
-  // Post-conditon
-  // TODO: Write post-condition
+  assert(budget >= 0);
+  // Post-condition
+  // Returns remaining budget after recursively "buying" gifts from the given wishlist
+
+  // Base cases
+  if (budget == 0 || wishlist.empty()) {
+    return budget;
+  }
 
   // TODO: Write algorithm
-  return 0;
+  return budget;
 }
 
 #ifndef TESTING
--- main_test.cpp
+++ main_test.cpp
@@ -2,6 +2,10 @@
 #include "gtest/gtest.h"
 
 // TODO: Write correct tests
-TEST(gifts_test, returns_0) {
-  EXPECT_EQ(gifts(), 0);
+TEST(gifts_test, zero_budget) {
+  EXPECT_EQ(gifts({"Playstation 5"}, 0), 0);
+}
+
+TEST(gifts_test, empty_wishlist) {
+  EXPECT_EQ(gifts({}, 3000), 3000);
 }
