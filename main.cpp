39d8739b9de643802585eb389b394a503cb3754b
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2021-12-15 10:56:09 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+int breadth_first(vector<vector<char>>& field)
+{
+    //Implement this function
+}
+
+int depth_first(vector<vector<char>>& field)
+{
+    //Implement this function
+}
+
+#ifndef TESTING
+int main()
+{
+    return 0;
+}
+#endif
