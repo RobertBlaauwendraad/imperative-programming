f3c9b1e3c3de1655f8b43718d4765c8d8c27bb04
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2021-12-15 11:46:05 +0100

    Part 1

--- main.cpp
+++ main.cpp
@@ -7,19 +7,50 @@
 
 using namespace std;
 
-int breadth_first(vector<vector<char>>& field)
-{
-    //Implement this function
+void output_puzzle(const vector<vector<char>>& field) {
+  // Precondition
+  assert(true);
+  // Post-condition
+  // Outputs the given field to the console
+
+  for (auto & i : field) {
+    for (char j : i)
+      cout << j << " ";
+    cout << endl;
+  }
 }
 
-int depth_first(vector<vector<char>>& field)
-{
-    //Implement this function
+vector<vector<char>> read_puzzle(const string& challenge_filename) {
+  // Precondition
+  assert(true);
+  // Post-condition
+  // Return the field read from the given file
+
+  ifstream input(challenge_filename);
+  vector<vector<char>> field;
+  string line;
+  while (getline(input, line)) {
+    vector<char> row = {line.begin(), line.end()};
+    field.push_back(row);
+  }
+  return field;
 }
+
+//int breadth_first(vector<vector<char>>& field)
+//{
+//    //Implement this function
+//}
+//
+//int depth_first(vector<vector<char>>& field)
+//{
+//    //Implement this function
+//}
 
 #ifndef TESTING
 int main()
 {
-    return 0;
+  vector<vector<char>> field = read_puzzle("challenge.0.0steps.txt");
+  output_puzzle(field);
+  return 0;
 }
 #endif
